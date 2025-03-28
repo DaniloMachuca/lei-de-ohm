8b37941d8c5ba7049c173da27e89478ef3d11cb4
Danilo Machuca de Souza <danilo.machuca111@gmail.com>
2025-03-27 23:15:36 -0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const watch = require("gulp-watch");
+
+function compileSass() {
+  return gulp
+    .src("src/styles/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("dist/styles"));
+}
+
+exports.watch = function () {
+  gulp.watch("src/styles/*.scss", compileSass);
+};
+
+exports.compileSass = compileSass;
